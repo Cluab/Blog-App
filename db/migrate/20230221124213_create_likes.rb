73fb3ac5d9f6dcf482231f36df72db77766cc18c
cluab <you@example.com>
2023-02-21 14:55:03 +0200

    add like table

---
+++ db/migrate/20230221124213_create_likes.rb
@@ -0,0 +1,8 @@
+class CreateLikes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :likes do |t|
+      t.references :users, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_122653) do
+ActiveRecord::Schema.define(version: 2023_02_21_124213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_likes_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +31,5 @@ ActiveRecord::Schema.define(version: 2023_02_21_122653) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "users", column: "users_id"
 end
