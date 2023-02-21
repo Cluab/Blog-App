71d7f3558ebc71f37ca1bd4e0d2d0b973b450557
cluab <you@example.com>
2023-02-21 15:20:50 +0200

    add posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_124213) do
+ActiveRecord::Schema.define(version: 2023_02_21_131259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2023_02_21_124213) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["users_id"], name: "index_likes_on_users_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +41,5 @@ ActiveRecord::Schema.define(version: 2023_02_21_124213) do
   end
 
   add_foreign_key "likes", "users", column: "users_id"
+  add_foreign_key "posts", "users", column: "users_id"
 end
