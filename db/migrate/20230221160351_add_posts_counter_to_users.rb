cb42bf5c941c4928071ab25b12041fffbfa51549
cluab <you@example.com>
2023-02-21 18:05:34 +0200

    update users table

---
+++ db/migrate/20230221160351_add_posts_counter_to_users.rb
@@ -0,0 +1,4 @@
+class AddPostsCounterToUsers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_140909) do
+ActiveRecord::Schema.define(version: 2023_02_21_160351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
