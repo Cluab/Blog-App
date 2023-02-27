3a3f9279637df55ba2bfc120c21ba4b242f4dd53
cluab <you@example.com>
2023-02-27 13:08:45 +0200

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/:id', to: 'users#show', as: 'user'  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
 end
