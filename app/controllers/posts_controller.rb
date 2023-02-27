a8ec39bd3f83c2e1e51f212874b1d320b484564b
cluab <you@example.com>
2023-02-27 15:51:44 +0200

    add fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where(author_id: params[:id])
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
-    @post = Post.find_by(id: params[:post_id])
-    @comments = Comment.where(post_id: params[:post_id])
+    @post = Post.find_by(id: params[:id])
+    @comments = Comment.where(post_id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/:id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:id/posts/:post_id', to: 'posts#show', as: 'user_post'
-  get 'users/index'
-  get 'users/:id', to: 'users#show', as: 'user'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
 end
