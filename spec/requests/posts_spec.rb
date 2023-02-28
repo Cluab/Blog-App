c2c85e13fc53cc5867fdc70f8cbe051d4b7437d4
cluab <you@example.com>
2023-02-28 13:30:03 +0200

    add index action tests for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
   describe 'GET /index' do
+    it 'returns the index templete' do
+      get user_posts_path(user_id: 1)
+      expect(response).to render_template(:index)
+    end
     it 'returns http success' do
       get user_posts_path(user_id: 1)
       expect(response).to have_http_status(:success)
+    end
+    it 'includes the correct placeholder text in the response body' do
+      get user_posts_path(user_id: 1)
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 end
