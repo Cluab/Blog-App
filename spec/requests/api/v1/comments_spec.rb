fdbab84d1f38ae3dfcaa08c0a8b3f09c469b6d38
cluab <you@example.com>
2023-03-11 17:57:46 +0200

    init Rswag and applied tests

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -1,7 +1,68 @@
 require 'rails_helper'
+require 'swagger_helper'
+# rubocop:disable Metrics/BlockLength
+RSpec.describe 'api/comments', type: :request do
+  fixtures :all
 
-RSpec.describe 'Api::V1::Comments', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  path '/api/users/{user_id}/posts/{post_id}/comments' do
+    parameter name: 'user_id', in: :path, type: :integer, description: 'User id'
+    parameter name: 'post_id', in: :path, type: :integer, description: 'Post id'
+
+    get('list comments') do
+      response(200, 'successful') do
+        let(:user_id) { users(:user1).id }
+        let(:post_id) { posts(:post1).id }
+
+        schema type: :array,
+               properties: {
+                 id: { type: :integer },
+                 text: { type: :string },
+                 created_at: { type: :string },
+                 updated_at: { type: :string },
+                 author_id: { type: :integer },
+                 post_id: { type: :integer }
+               }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
+
+    post('create comment') do
+      consumes 'application/json'
+      produces 'application/json'
+
+      let(:comment) { { text: 'My first comment' } }
+
+      parameter name: :comment, in: :body, schema: {
+        type: :object,
+        properties: {
+          text: { type: :string }
+        },
+        required: %i[comment text]
+      }
+
+      response(201, 'successful') do
+        let(:user_id) { users(:user1).id }
+        let(:post_id) { posts(:post1).id }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
+# rubocop:disable Metrics/BlockLength
--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe 'Api::V1::Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'api/posts', type: :request do
+  fixtures :all
+
+  path '/api/users/{user_id}/posts' do
+    parameter name: 'user_id', in: :path, type: :string, description: 'User id'
+    let(:user_id) { users(:user1).id }
+
+    get('list posts') do
+      produces 'application/json'
+
+      response(200, 'Returns all posts created by a given user.') do
+        schema type: :array,
+               properties: {
+                 id: { type: :integer },
+                 title: { type: :string },
+                 text: { type: :string },
+                 comments_counter: { type: :integer },
+                 likes_counter: { type: :integer },
+                 created_at: { type: :string },
+                 updated_at: { type: :string },
+                 author_id: { type: :integer }
+               }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+
+        run_test!
+      end
+    end
   end
 end
