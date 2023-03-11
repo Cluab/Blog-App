47418eac345debe8bd91af2534a8279847bfaa2f
cluab <you@example.com>
2023-03-11 13:42:28 +0200

    update api POST action

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,25 +1,29 @@
 class Api::V1::CommentsController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
     comments = Comment.where(post_id: params[:post_id])
     render json: comments
   end
 
   def create
-    @post = Post.find(params[:post_id])
-    @user = User.last
-    @comment = Comment.create( author: @user, post: Post.find(params[:post_id]), text: comment_params[:text])    
-    respond_to do |format|
-      if @comment.save
-        format.json { render json: @comment, status: :created }
-      else
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    request_body = JSON.parse(request.body.read)
+    comment = Comment.new(
+      author_id: current_user.id,
+      post_id: params[:post_id],
+      text: request_body['comment']['text']
+    )
+    if comment.save
+      render json: comment, status: :created
+    else
+      errors = comment.errors.full_messages
+      errors << 'Please provide a comment text.' if errors.include?("Text can't be blank")
+      render json: { errors: errors }, status: :unprocessable_entity
     end
   end
-  
+
   private
-  
-  def comment_params
+
+  def comment_body
     params.require(:comment).permit(:text)
   end
-end  
+end
